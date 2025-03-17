b0077e56bd47ecd0c63af1fc57a413fd6218542d
thienbanho <thienbanho@gmail.com>
2025-03-17 16:06:50 +0700

    test 4

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * comment
+ * comment to test after remove file test2
  */
 package org.springframework.samples.petclinic.customers;
 
