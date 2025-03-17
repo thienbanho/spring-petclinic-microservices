309895a9b8924d81af4991f306a787344d4ad6f7
Nguyễn Trần Đức Thiện <125042711+thienbanho@users.noreply.github.com>
2025-03-17 13:32:07 +0700

    Update VetsServiceApplication.java

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-add comment in test branch
+add comment in test branch 2nd time
  
  */
 package org.springframework.samples.petclinic.vets;
