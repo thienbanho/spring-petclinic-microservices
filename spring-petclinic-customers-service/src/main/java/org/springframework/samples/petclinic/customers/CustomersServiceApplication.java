67698a3f00f39c3e739bc5e05ef4e464f99ea326
thienbanho <thienbanho@gmail.com>
2025-04-13 22:32:29 +0700

    test webhook

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * test customer service 3
+ * test customer service 4
  */
 package org.springframework.samples.petclinic.customers;
 
